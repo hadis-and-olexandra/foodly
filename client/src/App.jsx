e5c6f5797c7e18950f46c15b070cfe2358a78144
Oleksandra Nikitenkova <nikitenkovaoleksandra@gmail.com>
2025-08-05 21:03:59 +0200

    done

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:5001/api/home")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message))
+      .catch((error) => console.error("Error:", error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Message from backend:</h1>
+      <p>{message || "Loading..."}</p>
+    </div>
+  );
+}
+
+export default App;
